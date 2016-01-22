8bfb87737afe7f9371642650b96fa162d7d7b581
danielgwj <danielgwj95@hotmail.com>
2016-01-21 19:57:34 -0800

    Deck & Base Game
    Added the deck count function
    Added some test (not complete)
    Added the base game to run AcesUp

--- Assignment1/src/main/java/models/Deck.java
+++ Assignment1/src/main/java/models/Deck.java
@@ -66,4 +66,17 @@ public class Deck {
         return (deck.length == 54);
     }
 
+    /**
+     * As cards are dealt from the deck, the number of cards left
+     * decreases.  This function returns the number of cards that
+     * are still left in the deck.  The return value would be
+     * 52 or 54 (depending on whether the deck includes Jokers)
+     * when the deck is first created or after the deck has been
+     * shuffled.  It decreases by 1 each time the dealCard() method
+     * is called.
+     */
+    public int cardsLeft() {
+        return deck.length - cardsUsed;
+    }
+
 } // end class Deck
