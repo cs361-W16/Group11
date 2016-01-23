c0177a99bc16d5dabd2dcd5d3c635e66d9a7bdea
danielgwj <danielgwj95@hotmail.com>
2016-01-22 23:47:39 -0800

    General Housekeeping
    Started to initialize the game
    Test cases were updated

--- Assignment1/src/main/java/models/Card.java
+++ Assignment1/src/main/java/models/Card.java
@@ -142,10 +142,7 @@ public class Card {
      */
     public String toString() {
         if (suit == JOKER) {
-            if (value == 1)
-                return "Joker";
-            else
-                return "Joker #" + value;
+            return "Joker";
         }
         else
             return getValueAsString() + " of " + getSuitAsString();
