3de148f89e08280885c13615fc531645e880607c
Shotaro Matsui <matsuis@oregonstate.edu>
2016-01-22 17:37:48 -0800

    Deal function
    Added Deal class.
    Added shuffle and fit function into Deck class.
    Modified Deck class constructor to call shuffle() when a Deck class
    instance is created.
    
    Added tests for my part.

--- Assignment1/src/test/java/controllers/ApiControllerDocTesterTest.java
+++ Assignment1/src/test/java/controllers/ApiControllerDocTesterTest.java
@@ -17,6 +17,7 @@
 package controllers;
 
 
+import models.Deal;
 import org.junit.Test;
 
 import ninja.NinjaDocTester;
@@ -124,5 +125,107 @@ public class ApiControllerDocTesterTest extends NinjaDocTester {
     public void testDeckCount2() {
         Deck deck = new Deck();
         //WILL BE CONTINUED ONCE DEAL FUNCTION IS OUT
+    }
+    @Test
+    public void testHasJoker(){
+        Deck deck = new Deck(true);
+        assertEquals(deck.hasJokers(), true);
+    }
+    @Test
+    public void testShuffle(){
+        Deck deck1 = new Deck(false);
+        Deck deck2 = new Deck(false);
+        deck1.shuffle();
+        boolean isSameOrder = true;
+        for(int i=0; i<deck1.getDeckLength(); i++){
+            if(deck1.hit() != deck2.hit()) isSameOrder = false;
+        }
+        assertEquals(isSameOrder, false);
+    }
+    @Test
+    public void testHitDecks(){
+        Deck deck = new Deck(false);
+        boolean returnNull = false;
+        for(int i=0; i< deck.getDeckLength(); i++)
+            if(deck.hit() == null) returnNull = true;
+        assertEquals(returnNull, false);
+    }
+    @Test
+    public void testHitEmptyDeck(){
+        Deck deck = new Deck(false);
+        for(int i=0; i< deck.getDeckLength(); i++) deck.hit();
+        assertEquals(deck.hit(), null);
+    }
+    @Test
+    public void testHitUniqueness(){//test for making sure that every hits are different cards
+        Deck deck = new Deck(false);
+        Card[] cards = new Card[deck.getDeckLength()];
+        boolean unique = true;
+        for(int i=0; i< deck.getDeckLength(); i++){
+            cards[i] = deck.hit();
+            for(int j=0; j<i; j++)
+                if(cards[i] == cards[j]) unique = false;
+        }
+        assertEquals(unique, true);
+    }
+    @Test
+    public void testgetTopRowNumber1(){
+        Card[][] board = new Card[1][2];
+        board[0][0] = new Card(3,3);
+        board[0][1] = null;
+        int[] x = Deal.getTopRows(board);
+        assertEquals(x[0], 0);
+    }
+    @Test
+    public void testgetTopRowNumber0(){
+        Card[][] board = new Card[1][2];
+        board[0][0] = null;
+        board[0][1] = new Card(3,3);
+        int[] x = Deal.getTopRows(board);
+        assertEquals(x[0], -1);
+    }
+    @Test
+    public void testDealToRow0(){
+        Deck deck = new Deck(false);
+        Card[][] board = new Card[1][2];
+        board[0][0] = null;
+        board[0][1] = null;
+        assertEquals(Deal.deal(deck, board)&&(board[0][0] != null), true);
+    }
+    @Test
+    public void testDealToRow1(){
+        Deck deck = new Deck(false);
+        Card[][] board = new Card[1][2];
+        board[0][0] = new Card(3,3);
+        board[0][1] = null;
+        assertEquals(Deal.deal(deck, board)&&(board[0][1] != null), true);
+    }
+    @Test
+    public void testDealTo2Columns(){
+        Deck deck = new Deck(false);
+        Card[][] board = new Card[2][3];
+        board[0][0] = new Card(3,3);    board[1][0] = new Card(4,3);
+        board[0][1] = null;             board[1][1] = new Card(2,2);
+        board[0][2] = null;             board[1][2] = null;
+        assertEquals(Deal.deal(deck, board)&&(board[0][1] != null)&&(board[1][2] != null), true);
+    }
+    @Test
+    public void testDealEmptyDeck(){
+        Deck deck = new Deck(false);
+        for(int i=0; i< deck.getDeckLength(); i++) deck.hit();
+        Card[][] board = new Card[2][3];
+        board[0][0] = new Card(3,3);    board[1][0] = new Card(4,3);
+        board[0][1] = null;             board[1][1] = new Card(2,2);
+        board[0][2] = null;             board[1][2] = null;
+        assertEquals(Deal.deal(deck, board)||(board[0][1] != null)||(board[1][2] != null), false);
+    }
+    @Test
+    public void testDealToFullBoard(){
+        Deck deck = new Deck(false);
+        Card[][] board = new Card[1][3];
+        board[0][0] = new Card(3,3);
+        board[0][1] = new Card(5,2);
+        board[0][2] = new Card(9,0);
+        assertEquals(Deal.deal(deck, board), false);
     }
 }
